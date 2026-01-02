2c9c2eb4c0f7ad9c1773c94229c0eb792b9f5ef6
David Larochelle <david.larochelle@gmail.com>
2026-01-02 14:13:38 -0500

    linting and .venv

--- BuildKML.py
+++ BuildKML.py
@@ -1,90 +1,96 @@
-'''
+"""
 Created on 29 juill. 2019
 
 @author: david.larochelle
-'''
+"""
+
 import sys
 import getopt
 import os
 from tkinter import filedialog
 from KML import KML
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pass
 
 # default value
-MinDistance = 2000
-InputPaths = ""
-KMLFileName = ""
+min_distance = 2000
+input_paths = ""
+kml_file_name = ""
 
 # If executed from command line, accept -d option to override MinDistance
 try:
     opts, args = getopt.getopt(sys.argv[1:], "hd:i:o:")
 except getopt.GetoptError:
-    print('test.py -d MinDistance -i InputPathsCommaSeparated -o OutputFile')
+    print("test.py -d MinDistance -i InputPathsCommaSeparated -o OutputFile")
     sys.exit(2)
 for opt, arg in opts:
-    if opt == '-h':
-        print('test.py -d MinDistance -i InputPathsCommaSeparated -o OutputFile')
+    if opt == "-h":
+        print("test.py -d MinDistance -i InputPathsCommaSeparated -o OutputFile")
         sys.exit()
     elif opt in ("-d"):
-        MinDistance = int(arg)
+        min_distance = int(arg)
     elif opt in ("-i"):
-        InputPaths = str(arg)
+        input_paths = str(arg)
     elif opt in ("-o"):
-        KMLFileName = str(arg)
-print ('Min Distance is ' + str(MinDistance) + " Meters.")
+        kml_file_name = str(arg)
+print("Min Distance is " + str(min_distance) + " Meters.")
 
 
 # Create KML object
-MyKML = KML("Unnamed")
+my_kml = KML("Unnamed")
 
 # Build a list of folders to scan if not provided on command line
-FolderList = []
-if (InputPaths == ""):
+folder_list = []
+if input_paths == "":
     while True:
-        Foldername = filedialog.askdirectory(title="Choose Folders to scan for picture files. Cancel when done.")
-        if (Foldername == ""):
+        foldername = filedialog.askdirectory(
+            title="Choose Folders to scan for picture files. Cancel when done."
+        )
+        if foldername == "":
             break
-        FolderList.append(Foldername)
+        folder_list.append(foldername)
 else:
     # Split InputPaths
-    FolderList = InputPaths.split(";")
-    
-if (len(FolderList) == 0):
+    folder_list = input_paths.split(";")
+
+if len(folder_list) == 0:
     # Canceled by user
     sys.exit()
 
 # Scan all selected foders for image files with GPS info
-NbFiles = 0
-for Foldername in FolderList:
-    print ("Scanning files in folder " + Foldername + " ...")
-    NbFiles += MyKML.ScanFolder(Folder=Foldername)
+nb_files = 0
+for foldername in folder_list:
+    print("Scanning files in folder " + foldername + " ...")
+    nb_files += my_kml.ScanFolder(folder=foldername)
 
-print ("Imported " + str(NbFiles) + " files from folders.")
+print("Imported " + str(nb_files) + " files from folders.")
 
-if (NbFiles == 0):
+if nb_files == 0:
     # Canceled by user
     print("No GPS info found in folder. Aborting.")
     sys.exit()
-    
-# FilteredQty = MyKML.FilterPlacemarks(MinDistance)
-MyKML.MinDistanceBetweenPlacemarks = MinDistance
+
+# FilteredQty = my_kml.FilterPlacemarks(min_distance)
+my_kml.min_distance_between_placemarks = min_distance
 
 # Export KML file
-if (KMLFileName == ""):
-    KMLFileName = filedialog.asksaveasfilename(title="Select KML output file", filetypes={("KML files", "*.kml")})
-file, ext = os.path.splitext(KMLFileName)
+if kml_file_name == "":
+    kml_file_name = filedialog.asksaveasfilename(
+        title="Select KML output file", filetypes={("KML files", "*.kml")}
+    )
+file, ext = os.path.splitext(kml_file_name)
 
-if (KMLFileName == ""):
+if kml_file_name == "":
     # Canceled by user
     print("No KML file provided. Aborting.")
     sys.exit()
-    
-# enforce kml extension
-if (ext.lower() != ".kml"):
-    KMLFileName = file + ".kml"
 
-_ , MyKML.MapName = os.path.split(KMLFileName)
-MyKML.MinDistanceBetweenPlacemark = MinDistance
-FileSaved = MyKML.SaveKMLFile(KMLFileName)
-print(KMLFileName + " exported successfully.")
+# enforce kml extension
+if ext.lower() != ".kml":
+    kml_file_name = file + ".kml"
+
+_ , my_kml.map_name = os.path.split(kml_file_name)
+my_kml.min_distance_between_placemarks = min_distance
+file_saved = my_kml.SaveKMLFile(kml_file_name)
+print(kml_file_name + " exported successfully.")
--- KML.py
+++ KML.py
@@ -3,9 +3,9 @@ Created on 29 juill. 2019
 
 @author: david.larochelle
 '''
+import math
 
-
-class KML(object):
+class KML():
     '''
     KML Class is created to handle scanning of multiple image files in multiple folders and ultimately export a KML file compatible
     with Google Maps import.
@@ -13,66 +13,66 @@ class KML(object):
     '''
     
     # Member variables
-    MapName = ""
-    MapDescription = ""
-    _PlacemarkList = []
-    _PlacemarkListIsOrdered = False
-    MinDistanceBetweenPlacemarks = 0  # minimum distance there as to be from previous point for current placemark to be exported.
+    map_name = ""
+    map_description = ""
+    _placemark_list = []
+    _placemark_list_is_ordered = False
+    min_distance_between_placemarks = 0  # minimum distance there as to be from previous point for current placemark to be exported.
     
-    def __init__(self, MapName:str):
+    def __init__(self, map_name:str):
         '''
         Constructor
         '''
-        self.MapName = MapName
+        self.map_name = map_name
     
     def __repr__(self):
-        return "KML({})".format(self.MapName)
+        return "KML({})".format(self.map_name)
     
     def __str__(self):
-        return "This is My KML : " + self.MapName
+        return "This is My KML : " + self.map_name
     
-    def ScanFolder(self, Folder:str):
+    def ScanFolder(self, folder:str):
         """
         Scans folders recursively to build a list of compatible image files, extract exif data and returns the number of imported files (that had gps information).
-        :param Folder: (string)
+        :param folder: (string)
         :rtype: int
         """
         # Get list of files
         from pathlib import Path
-        BaseFolder = Path(Folder)
-        FileTypes = ("*.jpg", "*.jpeg", "*.dng")
-        FileList = []
-        for FileType in FileTypes:
-            FileList.extend(BaseFolder.glob('**/' + FileType))  # # ** allows to parse forlder recursively
+        base_folder = Path(folder)
+        file_types = ("*.jpg", "*.jpeg", "*.dng")
+        file_list = []
+        for file_type in file_types:
+            file_list.extend(base_folder.glob('**/' + file_type))  # # ** allows to parse forlder recursively
         import exifread
         # import os
-        NbFiles = 0
-        for MyFile in FileList:
+        nb_files = 0
+        for my_file in file_list:
             try:
-                tags = exifread.process_file(open(str(MyFile), 'rb'))
-            except:
-                print("\tCould not read exif from file " + str(MyFile))
+                tags = exifread.process_file(open(str(my_file), 'rb'))
+            except Exception:
+                print("\tCould not read exif from file " + str(my_file))
                 
             try:
-                MyLat = self._convert_to_degress(tags['GPS GPSLatitude'])
+                my_lat = self._convert_to_degress(tags['GPS GPSLatitude'])
                 if (tags["GPS GPSLatitudeRef"].values[0] != "N"):
-                    MyLat = -MyLat
-                MyLon = self._convert_to_degress(tags['GPS GPSLongitude'])
+                    my_lat = -my_lat
+                my_lon = self._convert_to_degress(tags['GPS GPSLongitude'])
                 if (tags["GPS GPSLongitudeRef"].values[0] != "E"):
-                    MyLon = -MyLon
-                # FilePath , FileName = os.path.split(MyFile)
+                    my_lon = -my_lon
+                # FilePath , FileName = os.path.split(my_file)
                 # BasePath, FirstFolder = os.path.split(FilePath)
-                self._AddPlacemark(MyLat, MyLon, str(MyFile), Folder)
-                NbFiles += 1
+                self._AddPlacemark(my_lat, my_lon, str(my_file), folder)
+                nb_files += 1
                 
-            except:
-                print("\tCould not extract GPS info from file " + str(MyFile))
+            except Exception:
+                print("\tCould not extract GPS info from file " + str(my_file))
         
-        print(str(NbFiles) + " files have coordinates in folder " + Folder)
-        return NbFiles
+        print(str(nb_files) + " files have coordinates in folder " + folder)
+        return nb_files
     
-    def _AddPlacemark(self, Latitude:float, Longitude:float, Name:str="", Folder:str=""):
-        self._PlacemarkList.append({"Latitude":Latitude, "Longitude":Longitude, "Name":Name, "Folder":Folder})
+    def _AddPlacemark(self, latitude:float, longitude:float, name:str="", folder:str=""):
+        self._placemark_list.append({"Latitude":latitude, "Longitude":longitude, "Name":name, "Folder":folder})
         
     def _GetKMLString(self):
         
@@ -85,59 +85,59 @@ class KML(object):
         # create the file structure
         root = ET.Element('kml')
         root.set("xmlns", "http://www.opengis.net/kml/2.2")
-        Document = ET.SubElement(root, 'Document')
-        Name = ET.SubElement(Document, "name")
-        Name.text = self.MapName
-        description = ET.SubElement(Document, "description")
-        description.text = self.MapDescription
+        document = ET.SubElement(root, 'Document')
+        name = ET.SubElement(document, "name")
+        name.text = self.map_name
+        description = ET.SubElement(document, "description")
+        description.text = self.map_description
 
         # Sort List prior to export
         self._ReorderPlacemarks()
             
         # Create first folder element
-        Folder = ET.SubElement(Document, "Folder")
-        FolderName = ET.SubElement(Folder, "name")
-        FolderName.text = self._PlacemarkList[0]["Folder"]
+        folder = ET.SubElement(document, "Folder")
+        folder_name = ET.SubElement(folder, "name")
+        folder_name.text = self._placemark_list[0]["Folder"]
         
         import os
-        NbFilesToExport = 0
-        for MyCoord in self._PlacemarkList:
-            if (MyCoord["Folder"] != FolderName.text):
+        nb_files_to_export = 0
+        for my_coord in self._placemark_list:
+            if (my_coord["Folder"] != folder_name.text):
                 # Create new folder if different
-                Folder = ET.SubElement(Document, "Folder")
-                FolderName = ET.SubElement(Folder, "name")
-                FolderName.text = MyCoord["Folder"]
+                folder = ET.SubElement(document, "Folder")
+                folder_name = ET.SubElement(folder, "name")
+                folder_name.text = my_coord["Folder"]
                 
-            if (MyCoord["Export"]):
-                MyPlacemark = ET.SubElement(Folder, "Placemark")
-                if MyCoord["Name"] != "":
-                    FilePath , FileName = os.path.split(MyCoord["Name"])
-                    MyName = ET.SubElement(MyPlacemark, "name")
-                    MyName.text = FileName
-                    Mydesc = ET.SubElement(MyPlacemark, "description")
-                    Mydesc.text = FilePath
+            if (my_coord["Export"]):
+                my_placemark = ET.SubElement(folder, "Placemark")
+                if my_coord["Name"] != "":
+                    file_path, file_name = os.path.split(my_coord["Name"])
+                    my_name = ET.SubElement(my_placemark, "name")
+                    my_name.text = file_name
+                    my_desc = ET.SubElement(my_placemark, "description")
+                    my_desc.text = file_path
                 
-                MyPoint = ET.SubElement(MyPlacemark, "Point")
-                MyCoordinate = ET.SubElement(MyPoint, "coordinates")
-                MyCoordinate.text = str(MyCoord["Longitude"]) + "," + str(MyCoord["Latitude"])
-                NbFilesToExport += 1
+                my_point = ET.SubElement(my_placemark, "Point")
+                my_coordinate = ET.SubElement(my_point, "coordinates")
+                my_coordinate.text = str(my_coord["Longitude"]) + "," + str(my_coord["Latitude"])
+                nb_files_to_export += 1
         
-        print(str(NbFilesToExport) + " placemarks exported to KML.")
+        print(str(nb_files_to_export) + " placemarks exported to KML.")
         
         # create a new XML file with the results
-        XMLString = ET.tostring(root, encoding='unicode')
+        xml_string = ET.tostring(root, encoding='unicode')
         try:
-            XMLString = minidom.parseString(XMLString).toprettyxml()
-        except:
+            xml_string = minidom.parseString(xml_string).toprettyxml()
+        except Exception:
             print ("Problems converting to Pretty XML (minidom)...")
-        return XMLString
+        return xml_string
     
     def SaveKMLFile(self, path:str):
-        KMLString = self._GetKMLString()
+        kml_string = self._GetKMLString()
         try:
-            with open(path, "w") as MyFile:
-                MyFile.write(KMLString)
-        except:
+            with open(path, "w", encoding="utf-8") as my_file:
+                my_file.write(kml_string)
+        except Exception:
             print("\tCould not save KML file")
             return False
         
@@ -148,7 +148,7 @@ class KML(object):
         Sorts list based on Folder and latitude
         """
         # Sort List in order to calculate distance between points.
-        self._PlacemarkList = sorted(self._PlacemarkList, key=lambda i: (i["Folder"], i['Latitude']))
+        self._placemark_list = sorted(self._placemark_list, key=lambda i: (i["Folder"], i['Latitude']))
         self._FilterPlacemarks()  # recalculate placemarks to be exported
     
     def _FilterPlacemarks(self):
@@ -157,40 +157,41 @@ class KML(object):
         pMinDistance : distance in meters
         '''
         
-        print("\tMin Distance Between Placemarks set to " + str(self.MinDistanceBetweenPlacemarks) + " meters.")
+        print("\tMin Distance Between Placemarks set to " + str(self.min_distance_between_placemarks) + " meters.")
         
-        KeptCoords = []
+        kept_coords = []
 
-        ExportCount = 0
-        for MyCoord in self._PlacemarkList:
-            MyCoord["Export"] = True
-            for KeptCoord in KeptCoords:
-                if self._DistanceBetweenPlacemarks(MyCoord["Latitude"], MyCoord["Longitude"], KeptCoord["Latitude"], KeptCoord["Longitude"]) < self.MinDistanceBetweenPlacemarks:
-                    MyCoord["Export"] = False
+        export_count = 0
+        for my_coord in self._placemark_list:
+            my_coord["Export"] = True
+            for kept_coord in kept_coords:
+                if self._DistanceBetweenPlacemarks(my_coord["Latitude"], my_coord["Longitude"], kept_coord["Latitude"], kept_coord["Longitude"]) < self.min_distance_between_placemarks:
+                    my_coord["Export"] = False
                     break
             
-            if MyCoord["Export"] == True:
-                ExportCount += 1
-                KeptCoords.append(MyCoord)
+            if my_coord["Export"] == True:
+                export_count += 1
+                kept_coords.append(my_coord)
         
-        print("\t" + str(ExportCount) + " placemarks marked to export out of " + str(len(self._PlacemarkList)))
-        return  ExportCount
+        print("\t" + str(export_count) + " placemarks marked to export out of " + str(len(self._placemark_list)))
+        return  export_count
+    
+
         
-    def _convert_to_degress(self, value:float):
+    def _convert_to_degress(self, ratio) -> float:
         """
         Helper function to convert the GPS coordinates stored in the EXIF to degress in float format
         :param value:
         :type value: exifread.utils.Ratio
         :rtype: float
         """
-        d = float(value.values[0].num) / float(value.values[0].den)
-        m = float(value.values[1].num) / float(value.values[1].den)
-        s = float(value.values[2].num) / float(value.values[2].den)
+        d = float(ratio.values[0].num) / float(ratio.values[0].den)
+        m = float(ratio.values[1].num) / float(ratio.values[1].den)
+        s = float(ratio.values[2].num) / float(ratio.values[2].den)
     
         return d + (m / 60.0) + (s / 3600.0)
         
     def _DistanceBetweenPlacemarks(self, lat1:float, lon1:float, lat2:float, lon2:float):
-        import math
         R = 6378.137  # // Radius of earth in KM
         dLat = lat2 * math.pi / 180 - lat1 * math.pi / 180
         dLon = lon2 * math.pi / 180 - lon1 * math.pi / 180
